c850fb135de8170820a06d11dd36820c10f85550
imamoonakhter <imamoonakhter@gmail.com>
2025-03-04 22:21:51 +0500

    API execute in blog and slug

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,8 +1,15 @@
 import * as fs from 'fs';
 
-export default function handler(req, res) {
-  fs.readdir("blogdata", (err, data)=>{
-    res.status(200).json(data);
+export default async function handler(req, res) {
+  let data = await fs.promises.readdir("blogdata");
+  let allBlogs = [];
+  for (let index = 0; index < data.length; index++) {
+    const item = data[index];
+    let myfile = await fs.promises.readFile(('blogdata/'+item), 'utf-8')
+    allBlogs.push(JSON.parse(myfile))
+    
+  }
+  res.status(200).json(allBlogs)
 
-  })
+  
 } 
--- pages/blog.js
+++ pages/blog.js
@@ -1,24 +1,37 @@
-import React from 'react'
-import styles from "../styles/Blog.module.css"
-import Link from 'next/link'
+import React, { useEffect } from "react";
+import styles from "../styles/Blog.module.css";
+import Link from "next/link";
 
 //Step 1: Collect all the files from blogdata directory
 // Step2: Iterate through the and Display them
 const Blog = () => {
+  const [blog, setBlogs] = React.useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/api/blogs")
+      .then((a) => {
+        return a.json();
+      })
+      .then((parsed) => {
+        setBlogs(parsed);
+      });
+  }, []);
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-          <div className={styles.blogItem}>
-            <Link href={'/blogpost/learn-javascript'}>
-            <h2 className={styles.blogItemh3}>How to learn JavaScript in 2022?</h2>
-            </Link>
-            <p>JS us the language used to design logic for the web</p>
-          </div>
-          
-          
+        {blog.map((blogItem) => {
+          return (
+            <div className={styles.blogItem} key={blogItem.title}>
+              <Link href={`/blogpost/${blogItem.slug}`}>
+                <h2 className={styles.blogItemh3}>{blogItem.title}</h2>
+              </Link>
+              <p className={styles.blogItempara}>{blogItem.content.substr(0, 150)}...</p>
+            </div>
+          );
+        })}
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
